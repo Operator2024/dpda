f1d67a60b8fd36eae34ddeb6c66651898f084245
Vladimir Belomestnykh <work.pwnz+github@gmail.com>
2020-12-19 11:14:48 +0500

    DPDA. Bugfix. Added generator table of names and unoptimized code.

---
+++ codegen.py
@@ -0,0 +1,142 @@
+from json import load
+from re import findall
+from typing import Text, Dict, List, Any, Tuple
+
+
+def validator(g: Dict, verifyingstring: Text) -> Text:
+    cnt = 0
+    for i in g:
+        if isinstance(g.get(i), dict) is False:
+            if i == "LEFT":
+                verifyingstring += g["LEFT"]
+            elif i == "OPER":
+                verifyingstring += g["OPER"]
+            elif i == "RIGHT":
+                verifyingstring += g["RIGHT"]
+        elif isinstance(g.get(i), dict):
+            if g["LEVEL_ORIG"] != -1:
+                verifyingstring += "("
+            verifyingstring = validator(g[i], verifyingstring)
+            if g["LEVEL_ORIG"] != -1:
+                verifyingstring += ")"
+        cnt += 1
+    return verifyingstring
+
+
+def generatorofcode(graph: Dict, codebygraph: Dict):
+
+    for i in graph:
+        if isinstance(graph.get(i), dict) is False:
+            if i == "LEFT":
+                codebygraph[graph["LEVEL"]] = [f"LOAD {graph['LEFT']}", f"STORE ${graph['LEVEL']}"]
+            elif i == "OPER":
+                pass
+            elif i == "RIGHT":
+                if codebygraph.get(graph["LEVEL"]) is not None:
+                    # print(codebygraph[graph["LEVEL"]], "TEST", graph["LEVEL"], codebygraph)
+
+                    if len(codebygraph[graph["LEVEL"]]) == 1:
+                        codebygraph[graph["LEVEL"]].append(f"LOAD {graph['RIGHT']}")
+                        if graph["OPER"] == "*":
+                            codebygraph[graph["LEVEL"]].append(f"MPY ${graph['LEVEL'] + 1}")
+                        elif graph["OPER"] == "+":
+                            codebygraph[graph["LEVEL"]].append(f"ADD ${graph['LEVEL'] + 1}")
+                    elif len(codebygraph[graph["LEVEL"]]) > 1:
+                        codebygraph[graph["LEVEL"]].append(f"LOAD {graph['RIGHT']}")
+                        if graph["OPER"] == "*":
+                            codebygraph[graph["LEVEL"]].append(f"MPY ${graph['LEVEL']}")
+                        elif graph["OPER"] == "+":
+                            codebygraph[graph["LEVEL"]].append(f"ADD ${graph['LEVEL']}")
+
+                elif codebygraph.get(graph["LEVEL"]) is None:
+                    codebygraph[graph["LEVEL"]] = [f"LOAD {graph['LEFT']}", f"STORE ${graph['LEVEL']}"]
+
+        elif isinstance(graph.get(i), dict):
+            # a = 2 * ((TAX33X + 2) * (6 + TT))
+            # ["ADD $2", "ADD $3", "MPY $2"]
+            # level + 1
+            # ["LOAD 2", "STORE $0", "MPY $2", "MPY $0", "STORE a"]
+            # a = "variable=(2+((2+7)*PRICE2))*((TAX33X+2)*(6+TT))"
+            codebygraph = generatorofcode(graph[i], codebygraph)
+
+            if codebygraph.get(graph["LEVEL"]) is None:
+
+                if int(graph[i]["LEVEL"]) - 1 == int(graph["LEVEL"]):
+                    # l[graph["LEVEL"]] = [l[graph[i]['LEVEL']][len(l[graph[i]['LEVEL']])-1]]
+                    codebygraph[graph["LEVEL"]] = [codebygraph[graph[i]['LEVEL']]]
+                elif int(graph[i]["LEVEL"]) == int(graph["LEVEL"]) - 1:
+                    # l[graph["LEVEL"]] = [l[graph[i]['LEVEL']][len(l[graph[i]['LEVEL']]) - 1]]
+                    codebygraph[graph["LEVEL"]] = [codebygraph[graph[i]['LEVEL']]]
+            elif codebygraph.get(graph["LEVEL"]) is not None:
+                if len(codebygraph[graph["LEVEL"]]) == 1:
+                    # l[graph["LEVEL"]].append(l[graph[i]['LEVEL']][len(l[graph[i]['LEVEL']])-1])
+                    codebygraph[graph["LEVEL"]].append(codebygraph[graph[i]['LEVEL']])
+                    if graph["OPER"] == "*":
+                        # l[graph["LEVEL"]].append(f"MPY ${graph['LEVEL']+1}")
+                        codebygraph[graph["LEVEL"]].append(f"MPY {codebygraph[graph['LEVEL']][0][len(codebygraph[graph['LEVEL']][0]) - 1][4::]}")
+                    elif graph["OPER"] == "+":
+                        # l[graph["LEVEL"]].append(f"ADD ${graph['LEVEL']+1}")
+                        codebygraph[graph["LEVEL"]].append(f"ADD {codebygraph[graph['LEVEL']][0][len(codebygraph[graph['LEVEL']][0]) - 1][4::]}")
+                elif len(codebygraph[graph["LEVEL"]]) > 1:
+                    # l[graph["LEVEL"]].append(l[graph[i]['LEVEL']][len(l[graph[i]['LEVEL']]) - 1])
+                    codebygraph[graph["LEVEL"]].append(codebygraph[graph[i]['LEVEL']])
+                    if graph["OPER"] == "*":
+                        codebygraph[graph["LEVEL"]].append(f"MPY ${graph['LEVEL']}")
+                    elif graph["OPER"] == "+":
+                        codebygraph[graph["LEVEL"]].append(f"ADD ${graph['LEVEL']}")
+    return codebygraph
+
+
+def generatoroftablename(code: List, names: Text = None, number: Any = 0) -> Tuple[Text, Any]:
+    if names is None:
+        names = ""
+
+    for i in code:
+        if isinstance(i, list):
+            names, number = generatoroftablename(i, names, number)
+        elif isinstance(i, list) is False:
+            if findall("(LOAD\s(\d{1,}\.\d{1,}|\d{1,}|\w{1,})|STORE\s\w{1,})", i):
+                if "LOAD" in i:
+                    if findall("\d{1,}\.\d{1,}", i[5::]) and float(i[5::]):
+                        if not findall(f"{i[5::]}\sVariable", names):
+                            number += 1
+                            names += f"{number} {i[5::]} Variable, float type\n"
+                    elif findall("\w{1,}", i[5::]) and not i[5::].isdigit():
+                        if not findall(f"{i[5::]}\sVariable", names):
+                            number += 1
+                            names += f"{number} {i[5::]} Variable, string type\n"
+                    elif findall("\d{1,}", i[5::]) and i[5::].isdigit():
+                        if not findall(f"{i[5::]}\sVariable", names):
+                            number += 1
+                            names += f"{number} {i[5::]} Variable, integer type\n"
+                elif "STORE" in i:
+                    if findall("\w{1,}", i[6::]):
+                        if not findall(f"{i[6::]}\sVariable", names):
+                            number += 1
+                            names += f"{number} {i[6::]} Variable name\n"
+    return names, number
+
+
+if __name__ == '__main__':
+    with open("output.json", "r") as fthr:
+        jsonGraph: Dict = load(fthr)
+    # a = "variable=(2+((2+7)*PRICE2))*((TAX33X+2)*(6+TT))"
+
+    outputString: Text = ""
+    codeRaw: Dict = {}
+
+    kk = validator(jsonGraph, outputString)
+    print(kk)
+    ab = generatorofcode(jsonGraph, codeRaw)
+    print("======")
+    k = [x for x in ab.keys()]
+
+    ab[int("-1")] = [ab[int("-1")][2], f"STORE {jsonGraph['LEFT']}"]
+
+    bb, _ = generatoroftablename(ab[int("-1")])
+    print(bb)
+    # for i in sorted(k):
+    #     print(" " * i, ab[i], i)
+    # for i in sorted(k):
+    #     print(ab[i], i)
+    print(ab[int("-1")])
--- main.py
+++ main.py
@@ -1,117 +1,141 @@
-from typing import Text, Set as set_type, List, Tuple, Dict
-from re import *
 from copy import copy
 from json import dump
+from time import sleep
+from typing import Set as set_type
 
-def pda_alphavite(word: Text) -> set_type[Text]:
-    open_brackert = 0
-    close_bracket = 0
-    alphavite: set_type[Text] = {""}
+from codegen import *
+
+
+def inputverifier(word: Text) -> set_type[Text] or Text:
+    lBracket = 0
+    rBracket = 0
+    sigmaAlphabet: set_type = set()
     for i in word:
         if i != "(" and i != ")":
-            alphavite.add(i)
+            sigmaAlphabet.add(i)
         elif i == "(":
-            open_brackert += 1
+            lBracket += 1
         elif i == ")":
-            close_bracket += 1
+            rBracket += 1
 
-    if close_bracket != open_brackert:
-        return {"The total count of open brackets is not equal to the total count of closed brackets"}
+    if rBracket != lBracket:
+        return "The total count of open brackets is not equal to the total count of closed brackets"
     else:
-        return alphavite
+        return sigmaAlphabet
 
 
-def pda_graph(q: Text, symbol: Text, q_set: set_type, stack: List, alphavite: set_type[Text], level) -> Tuple:
+def dpda(q: Text, symbol: Text, stack: List, sigmaAlphabet: set_type[Text], level) -> Tuple:
     if q == "q0":
-        if "[a-zA-Z]" in alphavite:
+        if "[a-zA-Z]" in sigmaAlphabet:
             if findall("[a-zA-Z]", symbol) != []:
                 if "empty" in stack and len(stack) == 1:
-                    q_current = "q1"
-                    return q_current, stack, level
+                    qCurrentLocal = "q1"
+                    return qCurrentLocal, stack, level
     elif q == "q1":
-        if "[a-zA-Z]" in alphavite or "[0-9]" in alphavite :
+        if "[a-zA-Z]" in sigmaAlphabet or "[0-9]" in sigmaAlphabet:
             if findall("[a-zA-Z]", symbol) != [] or findall("[0-9]", symbol) != []:
                 if "empty" in stack and len(stack) == 1:
-                    q_current = "q1"
-                    return q_current, stack, level
+                    qCurrentLocal = "q1"
+                    return qCurrentLocal, stack, level
             else:
                 if symbol == "=":
-                    q_current ="q2"
-                    return q_current, stack, level + 1
+                    qCurrentLocal = "q2"
+                    return qCurrentLocal, stack, level + 1
     elif q == "q2":
-        if "(" not in alphavite and "(" == symbol:
+        if "(" not in sigmaAlphabet and "(" == symbol:
             stack.insert(0, "(")
-            q_current = "q3"
-            return q_current, stack, level + 1
-        elif "[0-9]" in alphavite and findall("[0-9]", symbol) != []:
-            q_current = "q4"
-            return q_current, stack, level
-        elif "[a-zA-Z]" in alphavite and findall("[a-zA-Z]", symbol) != []:
-            q_current = "q5"
-            return q_current, stack, level
+            qCurrentLocal = "q3"
+            return qCurrentLocal, stack, level + 1
+        elif "[0-9]" in sigmaAlphabet and findall("[0-9]", symbol) != []:
+            qCurrentLocal = "q4"
+            return qCurrentLocal, stack, level
+        elif "[a-zA-Z]" in sigmaAlphabet and findall("[a-zA-Z]", symbol) != []:
+            qCurrentLocal = "q5"
+            return qCurrentLocal, stack, level
     elif q == "q3":
-        if "(" not in alphavite and "(" == symbol:
+        if "(" not in sigmaAlphabet and "(" == symbol:
             stack.insert(0, "(")
-            q_current = "q3"
-            return q_current, stack, level + 1
-        elif "[0-9]" in alphavite and findall("[0-9]", symbol) != []:
-            q_current = "q4"
-            return q_current, stack, level
-        elif "[a-zA-Z]" in alphavite and findall("[a-zA-Z]", symbol) != []:
-            q_current = "q5"
-            return q_current, stack, level
+            qCurrentLocal = "q3"
+            return qCurrentLocal, stack, level + 1
+        elif "[0-9]" in sigmaAlphabet and findall("[0-9]", symbol) != []:
+            qCurrentLocal = "q4"
+            return qCurrentLocal, stack, level
+        elif "[a-zA-Z]" in sigmaAlphabet and findall("[a-zA-Z]", symbol) != []:
+            qCurrentLocal = "q5"
+            return qCurrentLocal, stack, level
         elif findall("(\*|\+)", symbol) != []:
             raise Exception("An arithmetic operation symbol can't appear after bracket and before a variable!")
     elif q == "q4":
-        if "[0-9]" in alphavite and findall("[0-9]", symbol) != []:
-            q_current = "q4"
-            return q_current, stack, level
-        elif "[0-9]" in alphavite and findall("\)", symbol) != []:
+        if "[0-9]" in sigmaAlphabet and findall("[0-9]", symbol) != []:
+            qCurrentLocal = "q4"
+            return qCurrentLocal, stack, level
+        elif "[0-9]" in sigmaAlphabet and findall("\)", symbol) != []:
             if len(stack) > 1 and "(" in stack:
-                q_current = "q6"
+                qCurrentLocal = "q6"
                 stack.pop(0)
-                return q_current, stack, level - 1
+                return qCurrentLocal, stack, level - 1
         elif findall("(\*|\+)", symbol) != []:
-            if "*" in alphavite and "+" in alphavite:
-                q_current = "q2"
-                return q_current, stack, level
+            if "*" in sigmaAlphabet and "+" in sigmaAlphabet:
+                qCurrentLocal = "q2"
+                return qCurrentLocal, stack, level
+        elif "." in sigmaAlphabet and findall("\.", symbol) != []:
+            qCurrentLocal = "q7"
+            return qCurrentLocal, stack, level
         elif findall("[a-zA-Z]", symbol) != []:
             raise Exception("Number can't be before a letter when declaring a variable!")
     elif q == "q5":
-        if "[0-9]" in alphavite and findall("[0-9]", symbol) != []:
-            q_current = "q5"
-            return q_current, stack, level
-        elif "[a-zA-Z]" in alphavite and findall("[a-zA-Z]", symbol) != []:
-            q_current = "q5"
-            return q_current, stack, level
-        elif "[0-9]" in alphavite and findall("\)", symbol) != [] or "[a-zA-Z]" in alphavite and findall("\)", symbol) != []:
+        if "[0-9]" in sigmaAlphabet and findall("[0-9]", symbol) != []:
+            qCurrentLocal = "q5"
+            return qCurrentLocal, stack, level
+        elif "[a-zA-Z]" in sigmaAlphabet and findall("[a-zA-Z]", symbol) != []:
+            qCurrentLocal = "q5"
+            return qCurrentLocal, stack, level
+        elif "[0-9]" in sigmaAlphabet and findall("\)", symbol) != [] or "[a-zA-Z]" in sigmaAlphabet and findall("\)", symbol) != []:
             if len(stack) > 1 and "(" in stack:
-                q_current = "q6"
+                qCurrentLocal = "q6"
                 stack.pop(0)
-                return q_current, stack, level - 1
+                return qCurrentLocal, stack, level - 1
         elif findall("(\*|\+)", symbol) != []:
-            if "*" in alphavite and "+" in alphavite:
-                q_current = "q2"
-                return q_current, stack, level
+            if "*" in sigmaAlphabet and "+" in sigmaAlphabet:
+                qCurrentLocal = "q2"
+                return qCurrentLocal, stack, level
     elif q == "q6":
-        if ")" not in alphavite and ")" == symbol:
+        if ")" not in sigmaAlphabet and ")" == symbol:
             if len(stack) > 1:
-                if stack[0] != empty_stack:
+                if stack[0] != e:
                     stack.pop(0)
-                    q_current = "q6"
-                    return q_current, stack, level - 1
+                    qCurrentLocal = "q6"
+                    return qCurrentLocal, stack, level - 1
             else:
                 raise Exception("Stack contains no paired brackets")
-        elif "[0-9]" in alphavite and findall("[0-9]", symbol) != []:
+        elif "[0-9]" in sigmaAlphabet and findall("[0-9]", symbol) != []:
             raise Exception("This state only supports the closing bracket or arithmetic operation symbol")
-        elif "[a-zA-Z]" in alphavite and findall("[a-zA-Z]", symbol) != []:
+        elif "[a-zA-Z]" in sigmaAlphabet and findall("[a-zA-Z]", symbol) != []:
             raise Exception("This state only supports the closing bracket or arithmetic operation symbol")
         elif findall("(\*|\+)", symbol) != []:
-            q_current = "q2"
-            return q_current, stack, level
+            qCurrentLocal = "q2"
+            return qCurrentLocal, stack, level
+    elif q == "q7":
+        if "." in sigmaAlphabet and findall("\.", symbol) != []:
+            raise Exception("The second dot is invalid!")
+        elif "[0-9]" in sigmaAlphabet and findall("[0-9]", symbol) != []:
+            qCurrentLocal = "q7"
+            return qCurrentLocal, stack, level
+        elif findall("\)", symbol) != []:
+            if len(stack) > 1 and "(" in stack:
+                qCurrentLocal = "q6"
+                stack.pop(0)
+                return qCurrentLocal, stack, level - 1
+        elif findall("(\*|\+)", symbol) != []:
+            if "*" in sigmaAlphabet and "+" in sigmaAlphabet:
+                qCurrentLocal = "q2"
+                return qCurrentLocal, stack, level
+        elif findall("[a-zA-Z]", symbol) != []:
+            raise Exception("Number can't be before a letter when declaring a variable!")
+
 
 # Запись по знаку, закрывающей скобке, либо по концу строки и пустоте стека
-def json_graph(level, x, graph, lexem, oper = None):
+def json_graph(level, x, graph, lexem, oper=None):
     a = "variable=((AB+2)*3)*(1+6)"
     # 1,0,..,+
 
@@ -119,7 +143,7 @@ def json_graph(level, x, graph, lexem, oper = None):
     print(total_level, "TESTDEBUG", graph.keys())
 
     if graph.get(f"{total_level}") is None:
-        graph_vertex = copy(graph_ll)
+        graph_vertex = copy(graphPattern)
         graph_vertex["LEVEL"] = total_level
         graph_vertex["LEVEL_ORIG"] = level
         graph_vertex["X"] = x
@@ -127,8 +151,8 @@ def json_graph(level, x, graph, lexem, oper = None):
             if lexem != "":
                 graph_vertex["LEFT"] = lexem
             else:
-                if graph.get(f"{total_level+1}") is not None:
-                    graph_vertex["LEFT"] = copy(graph[f"{total_level+1}"])
+                if graph.get(f"{total_level + 1}") is not None:
+                    graph_vertex["LEFT"] = copy(graph[f"{total_level + 1}"])
             if graph_vertex["OPER"] == "" and oper is not None:
                 graph_vertex["OPER"] = oper
 
@@ -142,7 +166,7 @@ def json_graph(level, x, graph, lexem, oper = None):
                         if graph_vertex["RIGHT"] != "":
                             pass
                         else:
-                            graph_vertex["RIGHT"] = copy(graph[f"{total_level+1}"])
+                            graph_vertex["RIGHT"] = copy(graph[f"{total_level + 1}"])
         elif oper is None and lexem != "":
             graph_vertex = copy(graph[f"{total_level}"])
             if str(graph_vertex["LEVEL"]) == f"{total_level}":
@@ -191,7 +215,7 @@ def json_graph(level, x, graph, lexem, oper = None):
                             graph_vertex["OPER"] = ""
                             graph_vertex["RIGHT"] = ""
 
-                            graph_vertex["LEFT"] = graph[f"{total_level-1}"]
+                            graph_vertex["LEFT"] = graph[f"{total_level - 1}"]
                             graph_vertex["OPER"] = oper
         else:
             pass
@@ -199,96 +223,122 @@ def json_graph(level, x, graph, lexem, oper = None):
     graph[f"{total_level}"] = copy(graph_vertex)
     return graph, x
 
+
 if __name__ == '__main__':
 
-    q_states: set_type = {"q0", "q1", "q2", "q3", "q4", "q5", "q6"}
-    q_start: Text = "q0"
-    q_end: set_type = {"q4", "q5", "q6"}
+    qStates: set_type = {"q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7"}
+    qStart: Text = "q0"
+    qEnd: set_type = {"q4", "q5", "q6", "q7"}
 
-    sigma_alphavite: set_type[Text] = {"[a-zA-Z]", "[0-9]", "+", "*", "."}
-    empty_stack: Text = "empty"
-    stack: List = [empty_stack]
+    sAlphabet: set_type[Text] = {"[a-zA-Z]", "[0-9]", "+", "*", "."}
+    e: Text = "empty"
+    stack: List = [e]
 
-    q_current = q_start
+    qCurrent = qStart
 
     graph: Dict = {}
-    graph_ll: Dict = {"LEVEL": -1, "LEVEL_ORIG": -1, "LEFT": "", "OPER": "", "RIGHT": "", "CODE": [], "X": 0}
-    asm_free_idx = 0
+    graphPattern: Dict = {"LEVEL": -1, "LEVEL_ORIG": -1, "LEFT": "", "OPER": "", "RIGHT": "", "CODE": [], "X": 0}
     level = -1
-    lexema: Text = ""
+    lexeme: Text = ""
     reset = False
 
-    # a = "variable=((AB+2)*3)*(1+6)"
-    a = "variable=(2+((2+7)*PRICE2))*((TAX33X+2)*(6+TT))"
-    cnt = 0
-    x = 0
-    for i in a:
+    userString: Text = input('Enter a string in this format, but without quotes "var=a+b":  ')
 
-        if reset:
-            lexema = ""
-            reset = False
+    if inputverifier(userString) != "The total count of open brackets is not equal to the total count of closed brackets":
+        # cnt - Simple counter
+        # x - nesting ratio to control the number of brackets and correct construction graph in json file.
+        # Nesting ratio to control the number of brackets and correct construction graph in json file.
+        # This coefficient will be more than 0 if definite nesting level already exists in dictionary (json).
+        # In order not overwrite existing nesting level i used this coefficient
+        # Example, variable=(a+b)*(c+6).
+        # Left and right parts have brackets that equal nesting level - 1.
+        # If i save (a+b) in nesting level 1, then (c+6) i can't save as nesting level 1 already usage, then i can use this coefficient and get a new nesting level different from 1.
+        cnt = 0
+        x = 0
+        for i in userString:
+            if reset:
+                lexeme: Text = ""
+                reset = False
 
-        if i == "=":
-            q_current, stack, level = pda_graph(q_current, i, q_states, stack, sigma_alphavite, level)
-            print(q_current, stack, level, i)
-            graph_vertex_ = copy(graph_ll)
-            if graph_vertex_["LEVEL"] == -1:
-                if graph_vertex_["LEFT"] == "":
-                    graph_vertex_["LEFT"] = lexema
-                    reset = True
-                    if graph_vertex_["OPER"] == "":
-                        graph_vertex_["OPER"] = "="
-                graph[f"-1"] = graph_vertex_
+            if i == "=":
+                qCurrent, stack, level = dpda(qCurrent, i, stack, sAlphabet, level)
+                print(qCurrent, stack, level, i)
+                graph_vertex_ = copy(graphPattern)
+                if graph_vertex_["LEVEL"] == -1:
+                    if graph_vertex_["LEFT"] == "":
+                        graph_vertex_["LEFT"] = lexeme
+                        reset = True
+                        if graph_vertex_["OPER"] == "":
+                            graph_vertex_["OPER"] = "="
+                    graph[f"-1"] = graph_vertex_
+            elif i == "(":
+                qCurrent, stack, level = dpda(qCurrent, i, stack, sAlphabet, level)
+                print(i, level)
+            elif i == ")":
+                old_level = level
+                qCurrent, stack, level = dpda(qCurrent, i, stack, sAlphabet, level)
+                print(i, level, lexeme)
 
-        elif i == "(":
-            q_current, stack, level = pda_graph(q_current, i, q_states, stack, sigma_alphavite, level)
-            print(i, level)
-
-        elif i == ")":
-            old_level = level
-            q_current, stack, level = pda_graph(q_current, i, q_states, stack, sigma_alphavite, level)
-            print(i, level, lexema)
-
-            graph, kx = json_graph(old_level, x, graph, lexema)
-            x = kx
-            if cnt + 1 == len(a):
-                if x != 0:
-                    graph[f"{level}"]["RIGHT"] = copy(graph[f"{old_level+x}"])
-                else:
-                    graph[f"{level}"]["RIGHT"] = copy(graph[f"{old_level}"])
-            reset = True
-
-        elif i == "*" or i == "+":
-            q_current, stack, level = pda_graph(q_current, i, q_states, stack, sigma_alphavite, level)
-            print(q_current, stack, level, i)
-
-            print(x, "X")
-            graph, kx = json_graph(level, x, graph, lexema, i)
-            x = kx
-            reset = True
-
-
-        else:
-            q_current, stack, level = pda_graph(q_current, i, q_states, stack, sigma_alphavite, level)
-            print(q_current, stack, level, i)
-
-            if cnt +1 == len(a):
-                lexema += i
-                graph, kx = json_graph(level, x, graph, lexema)
+                graph, kx = json_graph(old_level, x, graph, lexeme)
                 x = kx
-                # reset = True
+                if cnt + 1 == len(userString):
+                    if x != 0:
+                        graph[f"{level}"]["RIGHT"] = copy(graph[f"{old_level + x}"])
+                    else:
+                        graph[f"{level}"]["RIGHT"] = copy(graph[f"{old_level}"])
+                reset = True
+            elif i == "*" or i == "+":
+                qCurrent, stack, level = dpda(qCurrent, i, stack, sAlphabet, level)
+                print(qCurrent, stack, level, i)
+
+                print(x, "X")
+                graph, kx = json_graph(level, x, graph, lexeme, i)
+                x = kx
+                reset = True
+            else:
+                qCurrent, stack, level = dpda(qCurrent, i, stack, sAlphabet, level)
+                print(qCurrent, stack, level, i)
+
+                if cnt + 1 == len(userString):
+                    lexeme += i
+                    graph, kx = json_graph(level, x, graph, lexeme)
+                    x = kx
+                    # reset = True
+
+                else:
+                    lexeme += i
+
+            if cnt + 1 == len(userString):
+                graph["-1"]["RIGHT"] = copy(graph["0"])
+            cnt += 1
+
+        # for debug
+        # print(lexeme)
+        # print(graph["-1"])
+        # print(graph["4"])
+        if qCurrent in qEnd:
+            with open("output.json", "w") as wr:
+                dump(graph["-1"], wr, indent=1)
+        elif qCurrent not in qEnd:
+            print(f"The deterministic extrusion automaton has a current state {qCurrent}, which isn't in the set of final states")
+
+        if qCurrent in qEnd:
+            outputString: Text = ""
+            codeRaw: Dict = {}
+            if userString == validator(graph["-1"], outputString):
+                codeRaw = generatorofcode(graph["-1"], codeRaw)
+                codeRaw[int("-1")] = [codeRaw[int("-1")][2], f"STORE {graph['-1']['LEFT']}"]
+
+                table, _ = generatoroftablename(codeRaw[int("-1")])
+                print(table)
+                print(codeRaw[int("-1")])
+                with open("output.txt", "w", encoding="utf8") as wt:
+                    wt.write("Table of names\n" + table + "="*10 + "\n")
+                    wt.write("Unoptimized code - " + str(codeRaw[int("-1")]) + "\n")
 
             else:
-                lexema += i
+                pass
 
-        if cnt +1 == len(a):
-            graph["-1"]["RIGHT"] = copy(graph["0"])
-        cnt += 1
-
-    print(lexema)
-    print(graph["-1"])
-    print(graph["4"])
-
-    with open("output.json", "w") as wr:
-        dump(graph["-1"], wr, indent=1)
-
+    else:
+        print("The total count of open brackets is not equal to the total count of closed brackets")
+        sleep(5)
